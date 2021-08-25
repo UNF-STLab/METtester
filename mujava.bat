d67398f898bf61a5af3c1544570e61e23dd51dd9
ps073006 <engr.ps07@gmail.com>
2021-08-25 11:47:04 -0600

    version 1.0.1 added

---
+++ mujava.bat
@@ -0,0 +1,3 @@
+set CLASSPATH=%CLASSPATH%;.\mujava.jar;.\lib\openjava.jar;.\lib\tools.jar;.\lib\commons-io-2.4.jar;.\lib\junit-4.10.jar;.\lib\hamcrest-core-1.3.jar
+java mujava.cli.testnew -debug MethodCollection2 MethodCollection2
+java mujava.cli.genmutes -AOIS -ROR MethodCollection2
--- src/runEvosuite.bat
+++
@@ -1,2 +0,0 @@
-java -jar evosuite-1.0.6.jar  -class MethodCollection2 -Dtarget_method="MethodCollection2.add_values([I)I" -projectCP . -criterion weakmutation
-pause;
